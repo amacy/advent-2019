6f870a8ec5a160d21dcb6e11652a568fa0cdd7fc
amacy <aaronmacy@gmail.com>
2020-09-27 12:16:49 -0400

    day 1, part 2: solved

--- lib/day_01.rb
+++ lib/day_01.rb
@@ -4,8 +4,26 @@ class Day01
   end
 
   def self.fuel_sum
-    input = File.readlines("config/day_01.txt").inject(0) do |sum, line|
-      sum += fuel_needed(line.to_i)
+    parse_and_sum_file { |mass| fuel_needed(mass) }
+  end
+
+  def self.total_fuel_needed(mass)
+    mass = mass.dup
+    total = 0
+    loop do
+      return total if mass <= 0
+      mass = fuel_needed(mass)
+      total += [mass, 0].max
+    end
+  end
+
+  def self.total_fuel_sum
+    parse_and_sum_file { |mass| total_fuel_needed(mass) }
+  end
+
+  def self.parse_and_sum_file(&block)
+    File.readlines("config/day_01.txt").inject(0) do |sum, line|
+      sum += block.call(line.to_i)
     end
   end
 end
--- spec/day_01_spec.rb
+++ spec/day_01_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe Day01 do
       expect(Day01.fuel_sum).to eq 3305115
     end
   end
+
+  context "part 2" do
+    it "calculates teh fueld based on the mass and the mass of the fuel" do
+      expect(Day01.total_fuel_needed(14)).to eq 2
+      expect(Day01.total_fuel_needed(1969)).to eq 966
+      expect(Day01.total_fuel_needed(100756)).to eq 50346
+    end
+
+    it "returns the correct answers for the input file" do
+      expect(Day01.total_fuel_sum).to eq 4954799
+    end
+  end
 end
