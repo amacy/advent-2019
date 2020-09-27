acf5f83999ae0d85b168fe48489700dbb929458c
amacy <aaronmacy@gmail.com>
2020-09-27 13:40:05 -0400

    day 2, part 2: solved

--- lib/day_02.rb
+++ lib/day_02.rb
@@ -28,10 +28,22 @@ class Day02
     end
   end
 
-  def self.parse_file
+  def self.parse_file(noun=12, verb=2)
     list = File.readlines("config/day_02.txt")[0].split(",").map(&:to_i)
-    list[1] = 12
-    list[2] = 2
+    list[1] = noun
+    list[2] = verb
     list
+  end
+
+  def self.find_noun_and_verb_by_output(desired_output)
+    range = 0..99
+    range.each do |noun|
+      range.each do |verb|
+        input = parse_file(noun, verb)
+        if run_program(input)[0] == desired_output
+          return (100 * noun) + verb
+        end
+      end
+    end
   end
 end
--- spec/day_02_spec.rb
+++ spec/day_02_spec.rb
@@ -16,5 +16,8 @@ RSpec.describe Day02 do
   end
 
   context "part 2" do
+    it "returns the correct answers for the input file" do
+      expect(Day02.find_noun_and_verb_by_output(19690720)).to eq 8051
+    end
   end
 end
