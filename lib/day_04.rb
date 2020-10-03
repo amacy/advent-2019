d81e80629fd21f826cd503bcba1d401daa5ea55e
amacy <aaronmacy@gmail.com>
2020-10-03 15:39:11 -0400

    day 4, part 2: solved

--- lib/day_04.rb
+++ lib/day_04.rb
@@ -1,16 +1,30 @@
 class Day04
-  def self.possible_passwords(input="158126-624574")
-    input = input.split("-")
+  def self.possible_passwords_part_1
+    input = parse_input
 
     (input[0]..input[1]).select do |n|
-      valid_password?(n)
+      valid_password_part_1?(n)
     end.count
   end
 
-  def self.valid_password?(str)
+  def self.valid_password_part_1?(str)
     valid_length?(str) &&
       ascending?(str) &&
-      contains_double?(str)
+      contains_double_part_1?(str)
+  end
+
+  def self.possible_passwords_part_2
+    input = parse_input
+
+    (input[0]..input[1]).select do |n|
+      valid_password_part_2?(n)
+    end.count
+  end
+
+  def self.valid_password_part_2?(str)
+    valid_length?(str) &&
+      ascending?(str) &&
+      contains_double_part_2?(str)
   end
 
   def self.valid_length?(str)
@@ -28,12 +42,34 @@ class Day04
     end
   end
 
-  def self.contains_double?(str)
+  def self.contains_double_part_1?(str)
     index = 0
     loop do
       return false if index == str.length - 1
       break true if str[index] == str[index + 1]
       index += 1
     end
+  end
+
+  def self.contains_double_part_2?(str)
+    index = 0
+    loop do
+      return false if index == str.length - 1
+      next_index = index + 1
+      if str[index] == str[next_index]
+        prev_index = index - 1
+        next_next_index = next_index + 1
+        if prev_index < 0 || str[prev_index] != str[index]
+          if next_next_index >= str.length || str[next_next_index] != str[index]
+            return true
+          end
+        end
+      end
+      index += 1
+    end
+  end
+
+  def self.parse_input
+    "158126-624574".split("-")
   end
 end
--- spec/day_04_spec.rb
+++ spec/day_04_spec.rb
@@ -4,20 +4,35 @@ require "pry"
 RSpec.describe Day04 do
   context "part 1" do
     it "finds possible passwords" do
-      expect(Day04.valid_password?(11111)).to eq false
-      expect(Day04.valid_password?(1111111)).to eq false
-      expect(Day04.valid_password?(111111)).to eq true
-      expect(Day04.valid_password?(111123)).to eq true
-      expect(Day04.valid_password?(122345)).to eq true
-      expect(Day04.valid_password?(223450)).to eq false
-      expect(Day04.valid_password?(123789)).to eq false
+      expect(Day04.valid_password_part_1?("11111")).to eq false
+      expect(Day04.valid_password_part_1?("1111111")).to eq false
+      expect(Day04.valid_password_part_1?("111111")).to eq true
+      expect(Day04.valid_password_part_1?("111123")).to eq true
+      expect(Day04.valid_password_part_1?("122345")).to eq true
+      expect(Day04.valid_password_part_1?("223450")).to eq false
+      expect(Day04.valid_password_part_1?("123789")).to eq false
     end
 
     it "returns the correct answers for the input" do
-      expect(Day04.possible_passwords).to eq 1665
+      expect(Day04.possible_passwords_part_1).to eq 1665
     end
   end
 
   context "part 2" do
+    it "finds possible passwords" do
+      expect(Day04.valid_password_part_2?("12355")).to eq false
+      expect(Day04.valid_password_part_2?("1234566")).to eq false
+      expect(Day04.valid_password_part_2?("112233")).to eq true
+      expect(Day04.valid_password_part_2?("111122")).to eq true
+      expect(Day04.valid_password_part_2?("112222")).to eq true
+      expect(Day04.valid_password_part_2?("123345")).to eq true
+      expect(Day04.valid_password_part_2?("123444")).to eq false
+      expect(Day04.valid_password_part_2?("223450")).to eq false
+      expect(Day04.valid_password_part_2?("123789")).to eq false
+    end
+
+    it "returns the correct answers for the input" do
+      expect(Day04.possible_passwords_part_2).to eq 1131
+    end
   end
 end
