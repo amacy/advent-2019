95283bbfdca1580250a0110ee120c771720b8d31
amacy <aaronmacy@gmail.com>
2020-10-03 14:00:49 -0400

    day 3, part 2: solved

--- lib/day_03.rb
+++ lib/day_03.rb
@@ -8,6 +8,15 @@ class Day03
     end.min
   end
 
+  def self.wire_steps(path_1=parse_file[0], path_2=parse_file[1])
+    nodes_1 = to_nodes(path_1)
+    nodes_2 = to_nodes(path_2)
+    intersections = nodes_1 & nodes_2
+    intersections.map do |intersection|
+      nodes_1.find_index(intersection) + nodes_2.find_index(intersection) + 2
+    end.min
+  end
+
   def self.to_nodes(path)
     current_x = 0
     current_y = 0
@@ -16,25 +25,25 @@ class Day03
       distance = location[1..-1].to_i
       case location[0]
       when "U"
-        travel_range(current_y + 1, distance).each do |y|
+        travel_range(current_y, distance).each do |y|
           nodes << [current_x, y]
         end
 
         current_y += distance
       when "D"
-        travel_range(current_y - 1, -distance).each do |y|
+        travel_range(current_y, -distance).each do |y|
           nodes << [current_x, y]
         end
 
         current_y -= distance
       when "R"
-        travel_range(current_x + 1, distance).each do |x|
+        travel_range(current_x, distance).each do |x|
           nodes << [x, current_y]
         end
 
         current_x += distance
       when "L"
-        travel_range(current_x - 1, -distance).each do |x|
+        travel_range(current_x, -distance).each do |x|
           nodes << [x, current_y]
         end
 
@@ -46,9 +55,9 @@ class Day03
 
   def self.travel_range(start, distance)
     if distance < 0
-      (start + distance..start).to_a.reverse
+      (start + distance..start - 1).to_a.reverse
     else
-      start..start + distance
+      start + 1..start + distance
     end
   end
 
--- spec/day_03_spec.rb
+++ spec/day_03_spec.rb
@@ -3,7 +3,7 @@ require "pry"
 
 RSpec.describe Day03 do
   context "part 1" do
-    it "runs the program" do
+    it "finds the closest intersection" do
       expect(Day03.wire_distance(
         %w(R8 U5 L5 D3),
         %w(U7 R6 D4 L4),
@@ -27,11 +27,28 @@ RSpec.describe Day03 do
     end
 
     it "returns the correct answers for the input file" do
-      expect(Day03.wire_distance).to eq 135
+      expect(Day03.wire_distance).to eq 1264
     end
   end
 
   context "part 2" do
-    it "returns the correct answers for the input file"
+    it "finds the intersection that uses the least wire" do
+      expect(Day03.wire_steps(
+        %w(R8 U5 L5 D3),
+        %w(U7 R6 D4 L4),
+      )).to eq 30
+      expect(Day03.wire_steps(
+        %w(R75 D30 R83 U83 L12 D49 R71 U7 L72),
+        %w(U62 R66 U55 R34 D71 R55 D58 R83),
+      )).to eq 610
+      expect(Day03.wire_steps(
+        %w(R98 U47 R26 D63 R33 U87 L62 D20 R33 U53 R51),
+        %w(U98 R91 D20 R16 D67 R40 U7 R15 U6 R7),
+      )).to eq 410
+    end
+
+    it "returns the correct answers for the input file" do
+      expect(Day03.wire_steps).to eq 1264
+    end
   end
 end
