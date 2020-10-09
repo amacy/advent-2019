000ac33c3a5ab29d6efec724dbb4a5d00975d576
amacy <aaronmacy@gmail.com>
2020-10-08 21:32:27 -0400

    day 6, part 2: solved

--- lib/day_06.rb
+++ lib/day_06.rb
@@ -2,6 +2,38 @@ require "set"
 
 class Day06
   def self.orbits(input=parse_file)
+    result = 0
+    counted_nodes = Set.new
+    find_paths(input).each do |path|
+      loop do
+        node = path.shift
+        if counted_nodes.include?(node)
+          break
+        else
+          counted_nodes << node
+          result += path.length
+        end
+      end
+    end
+
+    result
+  end
+
+  def self.distance_from_santa(input=parse_file)
+    paths = find_paths(input).select do |path|
+      ["SAN", "YOU"].include?(path[0])
+    end
+
+    result = 0
+    paths[0].each_with_index do |node, index|
+      if paths[1].include?(node)
+        result = index + paths[1].index(node) - 2
+        return result
+      end
+    end
+  end
+
+  def self.find_paths(input)
     child_to_parent = {}
     parent_to_children = {}
     input.each do |pair|
@@ -31,22 +63,7 @@ class Day06
         current_node = child_to_parent[current_node]
       end
     end
-
-    result = 0
-    counted_nodes = Set.new
-    paths.each do |path|
-      loop do
-        node = path.shift
-        if counted_nodes.include?(node)
-          break
-        else
-          counted_nodes << node
-          result += path.length
-        end
-      end
-    end
-
-    result
+    paths
   end
 
   def self.parse_file
--- spec/day_06_spec.rb
+++ spec/day_06_spec.rb
@@ -14,8 +14,13 @@ RSpec.describe Day06 do
   end
 
   context "part 2" do
-    it "returns the correct answers for the input file" do
-      # expect(Day06.orbits([5])[0]).to eq 5000972
+    it "returns the correct distance for the sample input" do
+      input = %w[ COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L K)YOU I)SAN ]
+      expect(Day06.distance_from_santa(input)).to eq 241064
+    end
+
+    it "returns the correct distance for the input file" do
+      expect(Day06.distance_from_santa).to eq 241064
     end
   end
 end
